86d70294d510a6810eede205537999631c2c48be
Eklavya-San <eklavyaghodake@gmail.com>
2023-02-06 21:13:42 +0530

    del

--- Device_Manager/src/main/java/com/app/repo/DeviceRepo.java
+++
@@ -1,9 +0,0 @@
-package com.app.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Devices;
-
-public interface DeviceRepo extends JpaRepository<Devices, Long> {
-
-}
